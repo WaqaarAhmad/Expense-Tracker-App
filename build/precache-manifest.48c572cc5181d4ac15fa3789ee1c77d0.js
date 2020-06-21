7c32b24a6834097791e4068e411a4df1592dae2a
Waqar Ahmad <waqaar.ahmad@yahoo.com>
2020-06-21 18:17:57 +0500

    adding build folder to git

---
+++ build/precache-manifest.48c572cc5181d4ac15fa3789ee1c77d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522c5b091967d3fec9d9bfcb4624509a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91dc8c2d04c8e72b62ea",
+    "url": "/static/css/main.0ae3a5d6.chunk.css"
+  },
+  {
+    "revision": "0c775b68fcb5191b0f88",
+    "url": "/static/js/2.b3ae768b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3ae768b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dc8c2d04c8e72b62ea",
+    "url": "/static/js/main.1e981c7f.chunk.js"
+  },
+  {
+    "revision": "6a0ec6733e7cff7e13d9",
+    "url": "/static/js/runtime-main.21ea6443.js"
+  }
+]);
